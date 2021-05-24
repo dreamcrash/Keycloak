60793525c0120a6ad29c563f3906e1f79abbc802
Bruno Medeiros <lesiano16@gmail.com>
2021-05-24 14:56:23 +0200

    renaming variable

--- scripts/getUserByName.sh
+++ scripts/getUserByName.sh
@@ -15,17 +15,17 @@ KEYCLOAK_IP="$1"
 ADMIN_USERNAME="$2"
 ADMIN_PASSWORD="$3"
 REALM_NAME="$4"
-USER_NAME="$5"
+USERNAME="$5"
 SCRIPT_DIR="$(dirname "$0")"
 
 ADMIN_TOKEN="$($SCRIPT_DIR/getAdminToken.sh "$KEYCLOAK_IP" "$ADMIN_USERNAME" "$ADMIN_PASSWORD")"
 ACCESS_TOKEN="$(echo $ADMIN_TOKEN | jq -r .access_token)"
 
 
-USER=$(curl -k -sS -X GET "http://$KEYCLOAK_IP/auth/admin/realms/$REALM_NAME/users/?username=$USER_NAME" \
+USER=$(curl -k -sS -X GET "http://$KEYCLOAK_IP/auth/admin/realms/$REALM_NAME/users/?username=$USERNAME" \
 		-H "Content-Type: application/json" \
 		-H "Authorization: Bearer $ACCESS_TOKEN")
 
-echo $USER | jq -r ".[] | select(.username==\"$USER_NAME\")"
+echo $USER | jq -r ".[] | select(.username==\"$USERNAME\")"
 
 $SCRIPT_DIR/logoutAdminSession.sh "$KEYCLOAK_IP" "$ADMIN_TOKEN"
